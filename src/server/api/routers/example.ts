2ef75199475e372959216165574c99c6290a68d2
pedrokielma <peterkielma@gmail.com>
2023-07-21 17:09:58 +0100

    commiting changes

---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    return ctx.prisma.post.findMany();
+  }),
+});
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,12 +3,9 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { PrismaClient } from '@prisma/client';
 
-type Context = {
-    prisma: PrismaClient;
-  };
 
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }: { ctx: Context }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx?.prisma?.post?.findMany();
   }),
 });
